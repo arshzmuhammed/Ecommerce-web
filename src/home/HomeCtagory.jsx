51683650e0c079b05bff5fe1db5b2cb90152ce48
arshad <arshzmuhammed1@gmail.com>
2024-05-30 17:30:15 -0700

    changes

--- src/home/HomeCtagory.jsx
+++ src/home/HomeCtagory.jsx
@@ -1,85 +1,92 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
+import img01 from 'src/assets/images/category/01.jpg';
+import img02 from 'src/assets/images/category/02.jpg';
+import img03 from 'src/assets/images/category/03.jpg';
+import img04 from 'src/assets/images/category/04.jpg';
+import img05 from 'src/assets/images/category/05.jpg';
+import img06 from 'src/assets/images/category/06.jpg';
 
 const subTitle = "Choose Any Products";
 const title = "Buy Everything with Us";
 const btnText = "Get Started Now";
 
 const categoryList = [
-{
-imgUrl: 'src/assets/images/category/01.jpg',
-imgAlt: 'category rajibraj91 rajibraj',
-iconName: 'icofont-brand-windows',
-title: 'DSLR Camera',
-},
-{
-imgUrl: 'src/assets/images/category/02.jpg',
-imgAlt: 'category rajibraj91 rajibraj',
-iconName: 'icofont-brand-windows',
-title: 'Shoes',
-},
-{
-imgUrl: 'src/assets/images/category/03.jpg',
-imgAlt: 'category rajibraj91 rajibraj',
-iconName: 'icofont-brand-windows',
-title: 'Photography',
-},
-{
-imgUrl: 'src/assets/images/category/04.jpg',
-imgAlt: 'category rajibraj91 rajibraj',
-iconName: 'icofont-brand-windows',
-title: 'Formal Dress',
-},
-{
-imgUrl: 'src/assets/images/category/05.jpg',
-imgAlt: 'category rajibraj91 rajibraj',
-iconName: 'icofont-brand-windows',
-title: 'Colorful Bags',
-},
-{
-imgUrl: 'src/assets/images/category/06.jpg',
-imgAlt: 'category rajibraj91 rajibraj',
-iconName: 'icofont-brand-windows',
-title: 'Home Decor',
-},
-]
-const HomeCtagory = () => {
-    
+  {
+    imgUrl: img01,
+    imgAlt: 'DSLR Camera',
+    iconName: 'icofont-brand-windows',
+    title: 'DSLR Camera',
+  },
+  {
+    imgUrl: img02,
+    imgAlt: 'Shoes',
+    iconName: 'icofont-brand-windows',
+    title: 'Shoes',
+  },
+  {
+    imgUrl: img03,
+    imgAlt: 'Photography',
+    iconName: 'icofont-brand-windows',
+    title: 'Photography',
+  },
+  {
+    imgUrl: img04,
+    imgAlt: 'Formal Dress',
+    iconName: 'icofont-brand-windows',
+    title: 'Formal Dress',
+  },
+  {
+    imgUrl: img05,
+    imgAlt: 'Colorful Bags',
+    iconName: 'icofont-brand-windows',
+    title: 'Colorful Bags',
+  },
+  {
+    imgUrl: img06,
+    imgAlt: 'Home Decor',
+    iconName: 'icofont-brand-windows',
+    title: 'Home Decor',
+  },
+];
+
+const HomeCategory = () => {
   return (
     <div className="category-section style-4 padding-tb">
       <div className="container">
         <div className="section-header text-center">
-           <span className="subtitle">{subTitle}</span>
-           <h2 className="title">{title}</h2>
+          <span className="subtitle">{subTitle}</span>
+          <h2 className="title">{title}</h2>
         </div>
         <div className="section-wrapper">
-            <div className="row g-4 justify-content-center row-cols-md-3 row-cols-sm-2 row-cols-1">
-                {
-                categoryList.map((val , i) => (<div key={i}>
-                    <Link to='/shop' className="category-item">
-                        <div className="category-inner">
-                            <div className="category-thumb">
-                                <img src={val.imgUrl} alt="" />
-                            </div>
-                            <div className="category-content">
-                                <div className="cate-icon">
-                                    <i className={val.iconName}></i>
-                                </div>
-                                <Link to='/shop'><h6>{val.title }</h6></Link>
-                            </div>
+          <div className="row g-4 justify-content-center row-cols-md-3 row-cols-sm-2 row-cols-1">
+            {
+              categoryList.map((val, i) => (
+                <div key={i}>
+                  <Link to='/shop' className="category-item">
+                    <div className="category-inner">
+                      <div className="category-thumb">
+                        <img src={val.imgUrl} alt={val.imgAlt} />
+                      </div>
+                      <div className="category-content">
+                        <div className="cate-icon">
+                          <i className={val.iconName}></i>
                         </div>
-                    </Link>
+                        <Link to='/shop'><h6>{val.title}</h6></Link>
+                      </div>
+                    </div>
+                  </Link>
                 </div>
-                ))
-                }
-            </div>
-            <div className="text-center mt-5">
-                <Link to="/shop" className="lab-btn"><span>{btnText}</span></Link>
-                </div>
+              ))
+            }
+          </div>
+          <div className="text-center mt-5">
+            <Link to="/shop" className="lab-btn"><span>{btnText}</span></Link>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeCtagory
+export default HomeCategory;
